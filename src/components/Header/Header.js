7da06d23cefcbf0f01c30abc51d580eb1b1cab18
Mostafa <m.alsham627@gmail.com>
2021-05-03 16:19:33 -0700

    styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { ImSearch } from "react-icons/im";
 import { Jumbotron, Form } from "react-bootstrap";
+import { makeStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
 function Header({ handleSort, sortbyName }) {
 
---
+++ src/components/Header/Search.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function SearchField() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className={classes.margin}>
+        <Grid container spacing={1} alignItems="flex-end">
+          <Grid item>
+            <AccountCircle />
+          </Grid>
+          <Grid item>
+            <TextField id="input-with-icon-grid" label="With a grid" />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
